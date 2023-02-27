fe9a904f68567eeaf3d1010f0d07076b9c2e103e
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-26 20:25:17 -0800

    Set Up StartViewController
    Initial view, where the user can choose to begin the sorting ceremony

---
+++ Sorting Hat/View Controllers/StartViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Sorting Hat
+//
+//  Created by Kabir Dhillon on 2/26/23.
+//
+
+import UIKit
+
+/*
+ Need to do:
+    Views:
+    - Welcome
+    - Question & Answer screen x8
+    - Results
+ Models:
+ */
+
+class StartViewController: UIViewController {
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
