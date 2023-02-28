d6001bbc1ea03b66aa71d8d4739036179db86be0
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-27 21:22:31 -0800

    Set Up Learn More Segue
    Implemented segue to Wizarding World website for house information

--- Sorting Hat/View Controllers/HouseInfoWebViewController.swift
+++ Sorting Hat/View Controllers/HouseInfoWebViewController.swift
@@ -6,20 +6,39 @@
 //
 
 import UIKit
+import WebKit
 
-class HouseInfoWebViewController: UIViewController {
+class HouseInfoWebViewController: UIViewController, WKNavigationDelegate {
     
     var hogwartsHouse: HogwartsHouse!
-
+    
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        webView.navigationDelegate = self
+        setWebsite(house: hogwartsHouse)
     }
     
-    // use web view delegate and load correct page based on switch
-    
-
-    
-
+    func setWebsite(house: HogwartsHouse) {
+        let urlString: String!
+        
+        switch house {
+        case .Gryffindor:
+            urlString = "https://www.wizardingworld.com/collections/gryffindor"
+        case .Hufflepuff:
+            urlString = "https://www.wizardingworld.com/collections/hufflepuff"
+        case .Ravenclaw:
+            urlString = "https://www.wizardingworld.com/collections/ravenclaw"
+        case .Slytherin:
+            urlString = "https://www.wizardingworld.com/collections/slytherin"
+        }
+        
+        guard let url = URL(string: urlString) else { 
+            return
+        }
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
 }
